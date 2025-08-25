99e0000fd1b1b4ebac278933a12d618e1f089c5d
RollieGarcia0031 <garciarollie827@gmail.com>
2025-08-25 14:32:54 +0800

    init prereq

---
+++ src/util/Response.php
@@ -0,0 +1,14 @@
+<?php
+
+class Response {
+    public static function create(
+        bool $success,
+        string $message,
+        $data = null,
+        $code=400)
+    {
+        $code = $success ? 200 : $code;
+        http_response_code($code);
+        return json_encode(array("success" => $success, "message" => $message, "data" => $data));
+    }
+}
