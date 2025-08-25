34b1d5f2c63c319ee128b9ea71404db1271d2a36
RollieGarcia0031 <garciarollie827@gmail.com>
2025-08-25 14:33:29 +0800

    create db.php

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+function db_connect() {
+    $user = 'postgres';
+    $password = '123456';
+    $db = 'final_proj_dev';
+    $host = 'localhost';
+    $port = '5433';
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db";
+
+    try {
+        $pdo = new PDO($dsn, $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        return $pdo;
+    } catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
+    }
+}
