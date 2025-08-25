2313647dc24cd62c88e560bd98cb861c48a0f374
RollieGarcia0031 <garciarollie827@gmail.com>
2025-08-25 14:55:13 +0800

    init user registration

--- api/auth/register.php
+++ api/auth/register.php
@@ -1,1 +1,34 @@
 <?php
+require_once __DIR__ . '/../util/Response.php';
+require_once __DIR__ . '/../../src/model/User.php';
+require_once __DIR__ . '/../middleware/Cors.php';
+
+corsAllow();
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo Response::create(false, "Method not allowed", null, 405);
+    exit;
+}
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$name = $data['name'];
+$password = $data['password'];
+$email = $data['email'];
+$first_name = $data['first_name'];
+$last_name = $data['last_name'];
+
+$user = new User();
+
+try {
+    $user->setName($name);
+    $user->setEmail($email);
+    $user->setPassword($password);
+    $user->setFirstName($first_name);
+    $user->setLastName($last_name);
+} catch (Exception $e) {
+    echo Response::create(false, "Error: " . $e->getMessage(), null, 400);
+    exit;
+}
+
+echo $user->register();
--- src/model/User.php
+++ src/model/User.php
@@ -1,5 +1,47 @@
 <?php
+require_once __DIR__ . '/../util/Response.php';
+require_once __DIR__ . '/../util/DB.php';
 
 class User {
-    
+    private string $name;
+    private string $email;
+    private string $password;
+    private string $first_name;
+    private string $last_name;
+
+    public function setName(string $name) {
+        $this->name = $name;
+    }
+    public function setEmail(string $email){
+        $this->email = $email;
+    }
+    public function setPassword(string $password){
+        $this->password = $password;
+    }
+    public function setFirstName(string $first_name){
+        $this->first_name = $first_name;
+    }
+    public function setLastName(string $last_name){
+        $this->last_name = $last_name;
+    }
+
+    public function register(){
+        $conn = db_connect();
+        $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
+
+        $q = "INSERT INTO users (username, email, password_hash, first_name, last_name) VALUES (:name, :email, :password, :first_name, :last_name)";
+        
+        $stmt = $conn->prepare($q);
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':email', $this->email);
+        $stmt->bindParam(':password', $password_hash);
+
+        try {
+            $stmt->execute();
+            $new_id = $conn->lastInsertId();
+            return Response::create(true, "User registered successfully", $new_id);
+        } catch(PDOException $e) {
+            return Response::create(false, "Error: " . $e->getMessage(), null, 500); 
+        }
+    }
 }
